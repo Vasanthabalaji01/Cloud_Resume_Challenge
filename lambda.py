001b169086caee9f22dd3f8598296b6b845fe0f6
MrCloudExplorer <110226727+Vasanthabalaji01@users.noreply.github.com>
2024-01-28 06:08:31 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.table('cloud-resume')
+def lambda_handler(event, context):
+    response = table.get_item(key={
+        'id':'0'
+    })
+    views = response['Item']['views']
+    views = views + 1
+    print(views)
+    response = table.put_item(Item={
+        'id':'1',
+        'views':views
+    })
+    
+    return views
